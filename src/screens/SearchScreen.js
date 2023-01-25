df6d679eba69d74301eab16442d58bb5a1dac47d
Patricio <patricioprp06@gmail.com>
2023-01-24 21:00:04 -0300

    se termina una parte de input de busqueeda

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,44 @@
 import React, {useState} from "react";
 import { Text,View,StyleSheet } from "react-native";
-import  SearchBar from '../components/SearchBar'
+import  SearchBar from '../components/SearchBar';
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
     const [term,setTerm] = useState('');
-    return <View>
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState([]);
+
+    const searchApi = async(searchTerm) => {
+        console.log('ejecutando la api')
+        try{
+        const response = await yelp.get('/search',{
+            params: {
+                limit: 50,
+                term: searchTerm,
+                location: 'san jode'
+            }
+        });
+        setResults(response.data.businesses)
+        setErrorMessage('');
+        }catch(err){
+            setErrorMessage('Algo anda mal...');
+        }
+    };
+
+    //llamar a searchApi cuando el componente se renderiza por
+    //primera vez / mala eleccion
+    // searchApi('pasta')
+    return (
+    <View>
         <SearchBar 
         term={term} 
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit = {() => console.log('El termino fue enviado')}
+        onTermChange={setTerm}
+        onTermSubmit = {() => searchApi(term)}
         />
-        <Text>{term}</Text>
+        { errorMessage ? <Text>{errorMessage}</Text> : null}
+        <Text>Encontramos {results.length} resultados</Text>
     </View>
+    );
 }
 
 const styles = StyleSheet.create({
